2b37da8be326f23e672f04769ed17b83271c5c02
#Jiruu_246 <81064575+Jiruu246@users.noreply.github.com>
2024-10-04 15:11:01 +1000

    Website skeleton

--- SolBadge-Front-End/src/App.jsx
+++ SolBadge-Front-End/src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import SolanaLogo from '/Solana.svg'
 import './App.css'
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([
+    {title: 'NTF 1', subtitle: 'sample', description: 'more sample', date: '10/04/2024'},
+    {title: 'NTF 2', subtitle: 'sample', description: 'more sample', date: '10/04/2024'},])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api')
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error(error))
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://solana.com" target="_blank">
+          <img src={SolanaLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>SolBadge is here!</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input type="text" placeholder="Enter your wallet address" />
+        <button>
+          find NTFs
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <VerticalTimeline
+        layout="1-column-left"
+      >
+        {data.map((item, index) => (
+          <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            date={item.date}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            icon={<img src={reactLogo} alt="react logo" />}
+          >
+            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{item.subtitle}</h4>
+            <p>
+              {item.description}
+            </p>
+          </VerticalTimelineElement>))}
+      </VerticalTimeline>
     </>
   )
 }
