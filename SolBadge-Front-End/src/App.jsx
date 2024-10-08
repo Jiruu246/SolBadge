878b53ce9ad81502e78606777b55d28e31e5a21f
Thinh_Nguyen <thinhnguyen042002@gmail.com>
2024-10-08 19:53:15 +1100

    Adding error display and dynamic api end point

--- SolBadge-Front-End/src/App.jsx
+++ SolBadge-Front-End/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import RightArrow from './assets/maki_arrow.svg'
 import LoadingIcon from './assets/90-ring.svg'
 import './App.css'
@@ -36,52 +36,16 @@ function App() {
   const [focus, setFocus] = useState(false)
   const [address, setAddress] = useState('')
   const [nfts, setNFTs] = useState(null)
-  // const [nfts, setNFTs] = useState(GroupNFTsByDate([
-  //   {
-  //     "name": "DePIN Revolution 2024 - day 2",
-  //     "timestamp": 1724545405,
-  //     "description": "DePIN Revolution 2024 - day 2 - I was there!",
-  //     "address": "E8TukwBikJUguTQcQBTbAiRGpUMBPwGLhhCWppBHW9xQ",
-  //     "image": "https://arweave.net/3tflnppn4YGmxyIKoqva2kiVwm7yv5MJbAQRDTZA0_g",
-  //     "location": null
-  //   },
-  //   {
-  //     "name": "Breakpoint Singapore 2024 - POAP",
-  //     "timestamp": 1726914878,
-  //     "description": "This POAP collection marks your last Chomp at a RWE (Real World Event) powered by Chomp. It looks like you chomped a deck at Breakpoint Singapore 2024! To celebrate your contribution to building better information for us all, Chompy has gifted you one free in-Chomp answer reveal in the form of this POAP. 👾 Go test it out now at app.chomp.games!",
-  //     "address": "6KGyUsfQZozLkAVfKuyYuQQaPeqzs2z2SfU7UcF6szhd",
-  //     "image": "https://gateway.irys.xyz/uAkw5mShxxzhLNAfVjjgDwdXvjsfSe0u8AKGzENqydA",
-  //     "location": null
-  //   },
-  //   {
-  //     "name": "DePIN Revolution 2024 - day 1",
-  //     "timestamp": 1724466335,
-  //     "description": "DePIN Revolution 2024 - day 1 - I was there!",
-  //     "address": "5Ar9mr9vLEmvaSCY9ccPvE38bVSnnKuLURLTvu6Qx4zu",
-  //     "image": "https://arweave.net/0HQrwgcWs-_NO3Mk4AiJjt_nEZg9OQaJ7Hbpcwt_ix8",
-  //     "location": null
-  //   },
-  //   {
-  //     "name": "DePIN Revolution 2024 - day 3",
-  //     "timestamp": 1724628101,
-  //     "description": "DePIN Revolution 2024 - day 3 - I was there!",
-  //     "address": "2R743vkEkSBwKLwitQZHoLtutF1jr7haxLXgP937J7cM",
-  //     "image": "https://arweave.net/4WIUFEGcY86VJyeBGKdIQ-wrmXB6HNjRohDfmyxowo0",
-  //     "location": null
-  //   }
-  // ]))
   
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(false)
 
   const getNfts = async () => {
     setLoading(true)
     try {
-      const response = await axios.get('http://localhost:3003/nfts', {params: {addr: address}})
+      const response = await axios.get(import.meta.env.VITE_API_END_POINT, {params: {addr: address}})
       setNFTs(GroupNFTsByDate(response.data.data))
-      console.log(response.data.data)
     } catch (error) {
       setError(error.message)
-      console.error(error)
     } finally {
       setLoading(false)
     }
@@ -113,17 +77,23 @@ function App() {
           <img src={loading ? LoadingIcon : RightArrow } alt="button image" className="w-8"/>
         </button>
       </div>
-      {nfts !== null && <VerticalTimeline layout="1-column-left">
-        {Object.keys(nfts).map((date, index) => (
-          <VerticalTimelineElement key={index} icon={<p className="text-lg font-bold text-left">{date}</p>}>
-            <div className="flex flex-wrap gap-8">
-            {nfts[date].map((nft, index) => (
-              <NFTCard key={index} nft={nft}/>
-            ))}
-            </div>
-          </VerticalTimelineElement>
-        ))}
-      </VerticalTimeline>}
+      {error ? (
+        <div>
+          <p className="text-red-500 text-lg font-bold">{error}</p>
+        </div>
+      ) : (nfts !== null &&
+        <VerticalTimeline layout="1-column-left">
+          {Object.keys(nfts).map((date, index) => (
+            <VerticalTimelineElement key={index} icon={<p className="text-lg font-bold text-left">{date}</p>}>
+              <div className="flex flex-wrap gap-8">
+              {nfts[date].map((nft, index) => (
+                <NFTCard key={index} nft={nft}/>
+              ))}
+              </div>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      )}
     </>
   )
 }
